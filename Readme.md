9230e43ecb832a0285b373d8deba4893a5e481e2
Kaustav Ghosh <teetangh@gmail.com>
2022-10-04 20:44:32 +0530

    added prisma graphql files

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Blogging App
+
+## Commands
+
+### Prisma command used to push the schema to the database
+
+```
+npx prisma db push --preview-feature
+```
--- next-graphl-prisma-blog/next.config.js
+++ next-graphl-prisma-blog/next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/api/graphql",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,locale",
+            // value:
+            //   "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
