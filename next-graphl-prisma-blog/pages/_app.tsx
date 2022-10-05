2cb25530ae0926b1d28c1a58531990b71a02fcce
Kaustav Ghosh <teetangh@gmail.com>
2022-10-05 11:15:33 +0530

    updated components

--- next-graphl-prisma-blog/pages/_app.tsx
+++ next-graphl-prisma-blog/pages/_app.tsx
@@ -1,17 +1,29 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+// import "../styles/globals.css";
+import "antd/dist/antd.css";
+
+import { Session } from "next-auth";
+import Layout from "../components/Layout";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: "/api/graphql",
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <ApolloProvider client={client}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </SessionProvider>
   );
 }
 
